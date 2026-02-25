90eea2d5265b3a7acfcad4736dca74c27a1c7cf8
Arvind dhage <arvinddhage35@gmail.com>
2026-02-25 17:57:23 +0530

    ControlStatment

---
+++ ControlStatment/src/Whileloop.java
@@ -0,0 +1,9 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=2){
+            System.out.println("Arvind"+i);
+            i++;
+        }
+    }
+}
