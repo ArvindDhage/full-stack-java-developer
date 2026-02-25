9534eaa8f7578e6c8e66018f8891cff84dbc62e9
Arvind dhage <arvinddhage35@gmail.com>
2026-02-25 17:21:09 +0530

    Conditional Statment

---
+++ Conditional Statement/src/IfElseStatment.java
@@ -0,0 +1,14 @@
+public class IfElseStatment {
+    public static void main(String [] args){
+        int pass =12345;
+        System.out.println("Now if is working ");
+        if(pass == pass){
+            System.out.println("pass");
+        }
+        else{
+
+            System.out.println("fail");
+        }
+        }
+    }
+
---
+++ Conditional Statement/src/Nestedelseif.java
@@ -0,0 +1,16 @@
+public class Nestedelseif {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 20;
+
+        // Outer if condition
+        if (a == 10) {
+
+            // Inner if condition
+            if (b == 20) {
+                System.out.println("nested if ");
+            }
+        }
+    }
+    }
+
