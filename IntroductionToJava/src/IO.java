172d57b4761108c677a88bca0c777805db96e65e
Arvind dhage <arvinddhage35@gmail.com>
2026-02-25 18:39:03 +0530

    input Syntax

---
+++ IntroductionToJava/src/IO.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class IO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        float y = sc.nextFloat();
+        double z = sc.nextDouble();
+        long a = sc.nextLong();
+        short b = sc.nextShort();
+        String s = sc.next();
+    }
+}
