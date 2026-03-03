d874df23e6c40e9abe00c370e43d7e330f35dbc1
Arvind Dhage <149325897+ArvindDhage@users.noreply.github.com>
2026-03-03 17:34:22 +0530

    Delete ControlStatment directory
    All file moved on the core java folder

--- ControlStatment/src/Whileloop.java
+++
@@ -1,9 +0,0 @@
-public class Whileloop {
-    public static void main(String[] args) {
-        int i=1;
-        while(i<=2){
-            System.out.println("Arvind"+i);
-            i++;
-        }
-    }
-}
