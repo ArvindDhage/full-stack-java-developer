61fbd7d26c95999dcccbabc8e9c893d84ef03c26
Arvind Dhage <149325897+ArvindDhage@users.noreply.github.com>
2026-03-03 17:34:53 +0530

    Delete JumpStatment directory
    All file moved on the core java folder

--- JumpStatment/src/ReturnStatement.java
+++
@@ -1,8 +0,0 @@
-public class ReturnStatement {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        return;
-        // System.out.println("World");  // Error (unreachable code)
-    }
-}
-
